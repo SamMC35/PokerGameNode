1389a9b7f096a4c59292dc96ab843f6383e70385
SamMC35 <sambaditya.35@gmail.com>
2025-08-29 12:32:28 +0000

    added more hands

--- src/service/handService.js
+++ src/service/handService.js
@@ -3,26 +3,52 @@ import SUIT from '../entities/suit.js'
 import RANK from '../entities/rank.js'
 import HAND from '../entities/hand.js'
 
+import { sort } from 'fast-sort';
+
 export function calculateHand(decks) {
   //Check royal flush 
 
-  //Check one pair 
-  var pairs = checkPairs(decks) 
-  if(pairs == 2){
+  //check straight 
+  if (checkStraight(decks)) {
+
+  }
+  //Check three of a kind 
+  if (checkThreeOfAKind(decks)) {
+    return HAND.THREE_OF_A_KIND;
+  }
+
+  //Check pair 
+  var pairs = checkPairs(decks)
+  if (pairs == 2) {
     return HAND.TWO_PAIR;
-  } else if(pairs == 1){
+  } else if (pairs == 1) {
     return HAND.ONE_PAIR;
   }
 
   return HAND.HIGH_CARD;
 }
 
-function checkThreeOfAKind(decks){
+function checkStraight(decks) {
 
 }
 
-function checkPairs(decks) {
+function checkThreeOfAKind(decks) {
   var countMap = {}
+
+  countPairs(countMap, decks)
+
+  var threeOfAKind = 0
+
+  countMap.forEach((entry) => {
+    if (entry.value == 3) {
+      threeOfAKind += 1;
+    }
+  })
+
+  return threeOfAKind == 1 ? true : false;
+}
+
+function countPairs(countMap, decks) {
   decks.forEach(card => {
     if (!countMap[card.rank]) {
       countMap.set(card.rank, 1);
@@ -31,6 +57,12 @@ function checkPairs(decks) {
       countMap.set(card.rank, count + 1)
     }
   })
+}
+
+function checkPairs(decks) {
+  var countMap = {}
+
+  countPairs(countMap, decks)
 
   var pairs = 0;
 
